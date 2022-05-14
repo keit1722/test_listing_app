0f2b8b02a137fee9730d527d02d21c119f475bc6
keit1722 <keit1722@gmail.com>
2022-05-15 03:35:18 +0900

    [add]Usersコントローラーを作成

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,30 @@
+class UsersController < ApplicationController
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      redirect_to root_path, success: 'アカウント登録に成功しました'
+    else
+      flash.now[:error] = 'アカウント登録に失敗しました'
+      render :new
+    end
+  end
+
+  private
+
+  def user_params
+    params
+      .require(:user)
+      .permit(
+        :first_name,
+        :last_name,
+        :email,
+        :password,
+        :password_confirmation,
+        :slug,
+      )
+  end
+end
