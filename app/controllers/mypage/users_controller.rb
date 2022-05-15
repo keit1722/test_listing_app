72fe0957a1081256c960c442a1b32e3926a63c53
keit1722 <keit1722@gmail.com>
2022-05-16 01:38:18 +0900

    [add]Mypage::Usersコントローラーを追加

---
+++ app/controllers/mypage/users_controller.rb
@@ -0,0 +1,42 @@
+class Mypage::UsersController < ApplicationController
+  layout 'mypage'
+
+  def show
+    @user = User.find_by(public_uid: params[:id])
+  end
+
+  def edit
+    @user = User.find_by(public_uid: params[:id])
+  end
+
+  def update
+    @user = User.find_by(public_uid: params[:id])
+    if @user.update(user_params)
+      redirect_to root_path, success: 'プロフィールを更新しました'
+    else
+      flash.now[:error] = 'プロフィール更新ができませんでした'
+      render :edit
+    end
+  end
+
+  def destroy
+    @user = User.find_by(public_uid: params[:id])
+    @user.destroy!
+    redirect_to root_path, success: 'アカウントを削除しました'
+  end
+
+  private
+
+  def user_params
+    params
+      .require(:user)
+      .permit(
+        :first_name,
+        :last_name,
+        :email,
+        :password,
+        :password_confirmation,
+        :username,
+      )
+  end
+end
