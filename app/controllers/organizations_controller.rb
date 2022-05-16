aaa6e2a1d2f7cebbd6add778cc5d2038c15026c8
keit1722 <keit1722@gmail.com>
2022-05-17 06:59:54 +0900

    [add]organizationsコントローラーを作成

---
+++ app/controllers/organizations_controller.rb
@@ -0,0 +1,55 @@
+class OrganizationsController < ApplicationController
+  before_action :require_login
+
+  def index
+    @organizations = current_user.organizations
+  end
+
+  def new
+    @organization = current_user.organizations.build
+  end
+
+  def create
+    @organization = current_user.organizations.new(organization_new_params)
+    if @organization.save
+      redirect_to organizations_path, success: '組織を作成しました'
+    else
+      flash.now[:danger] = '組織を作成できませんでした'
+      render :new
+    end
+  end
+
+  def edit
+    @organization = current_user.organizations.find_by!(slug: params[:slug])
+  end
+
+  def update
+    @organization = current_user.organizations.find_by!(slug: params[:slug])
+    if @organization.update(organization_edit_paramas)
+      redirect_to organization_path, success: '組織の情報を更新しました'
+    else
+      flash.now[:error] = '組織の情報を更新できませんでした'
+      render :edit
+    end
+  end
+
+  def show
+    @organization = current_user.organizations.find_by!(slug: params[:slug])
+  end
+
+  def destroy
+    @organization = current_user.organizations.find_by!(slug: params[:slug])
+    @organization.destroy!
+    redirect_to organizations_path, success: '組織を削除しました'
+  end
+
+  private
+
+  def organization_new_params
+    params.require(:organization).permit(:name, :address, :phone, :slug)
+  end
+
+  def organization_edit_params
+    params.require(:organization).permit(:name, :address, :phone)
+  end
+end
