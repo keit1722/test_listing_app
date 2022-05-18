cdd3ae9e4714d00bbe162bf3f02d5e832f615147
keit1722 <keit1722@gmail.com>
2022-05-19 06:09:42 +0900

    [add]organization#restaurantsコントローラーを実装

--- app/controllers/organization/restaurants_controller.rb
+++ app/controllers/organization/restaurants_controller.rb
@@ -1,2 +1,86 @@
 class Organization::RestaurantsController < ApplicationController
+  layout 'mypage'
+
+  def index
+    @restaurants =
+      current_user
+        .organizations
+        .find_by!(slug: params[:organization_slug])
+        .restaurants
+  end
+
+  def show
+    @restaurant =
+      current_user
+        .organizations
+        .find_by!(slug: params[:organization_slug])
+        .restaurants
+        .find_by!(slug: params[:slug])
+  end
+
+  def new
+    @restaurant =
+      current_user
+        .organizations
+        .find_by!(slug: params[:organization_slug])
+        .restaurants
+        .build
+  end
+
+  def create
+    @restaurant =
+      current_user
+        .organizations
+        .find_by!(slug: params[:organization_slug])
+        .restaurants
+        .build(restaurant_create_params)
+    if @restaurant.save
+      redirect_to organization_restaurants_path, success: '登録しました'
+    else
+      flash.now[:error] = '登録に失敗しました'
+      render :new
+    end
+  end
+
+  def edit
+    @restaurant =
+      current_user
+        .organizations
+        .find_by!(slug: params[:organization_slug])
+        .restaurants
+        .find_by(slug: params[:slug])
+  end
+
+  def update
+    @restaurant =
+      current_user
+        .organizations
+        .find_by!(slug: params[:organization_slug])
+        .restaurants
+        .find_by!(slug: params[:slug])
+    if @restaurant.update(restaurant_update_params)
+      redirect_to organization_restaurant_path(
+                    @restaurant.organization,
+                    @restaurant,
+                  ),
+                  success: '情報を更新しました'
+    else
+      flash.now[:error] = '情報の更新に失敗しました'
+      render :edit
+    end
+  end
+
+  private
+
+  def restaurant_create_params
+    params
+      .require(:restaurant)
+      .permit(:name, :lat, :lng, :slug, :description, :address)
+  end
+
+  def restaurant_update_params
+    params
+      .require(:restaurant)
+      .permit(:name, :lat, :lng, :description, :address)
+  end
 end
