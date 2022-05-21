58b67b889a506d0d025e4e105a9dcf3429f5895d
keit1722 <keit1722@gmail.com>
2022-05-21 11:10:46 +0900

    [add]アクションによってlayoutを切り替えるよう設定

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,9 +1,17 @@
 class RestaurantsController < ApplicationController
+  layout :determine_layout
+
   def index
     @restaurants = Restaurant.all.order(created_at: :desc)
   end
 
   def show
     @restaurant = Restaurant.with_attached_images.find_by(slug: params[:slug])
+  end
+
+  private
+
+  def determine_layout
+    action_name == 'index' ? 'listing_index' : 'listing_single'
   end
 end
