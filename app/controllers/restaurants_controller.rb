d53571c5d3f81cef4cd0bea7c5abdcb5049b5c8e
keit1722 <keit1722@gmail.com>
2022-05-21 09:02:27 +0900

    [add]restaurantsコントローラーを作成

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,2 +1,9 @@
 class RestaurantsController < ApplicationController
+  def index
+    @restaurants = Restaurant.all.order(created_at: :desc)
+  end
+
+  def show
+    @restaurant = Restaurant.with_attached_images.find_by(slug: params[:slug])
+  end
 end
