34322581b2b1102f42a305ad9c0dfde26d8c2c67
keit1722 <keit1722@gmail.com>
2022-05-22 01:28:29 +0900

    [add]shopsコントローラーを作成

--- app/controllers/shops_controller.rb
+++ app/controllers/shops_controller.rb
@@ -1,2 +1,9 @@
 class ShopsController < ApplicationController
+  def index
+    @shops = Shop.all.order(created_at: :desc)
+  end
+
+  def show
+    @shop = Shop.with_attached_images.find_by(slug: params[:slug])
+  end
 end
