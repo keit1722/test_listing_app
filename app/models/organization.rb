a21a690946c249ad90bfb9018b424ecda8f654bb
keit1722 <keit1722@gmail.com>
2022-05-23 06:48:14 +0900

    [add]PhotoSpotとOrganizationを関連付け

--- app/models/organization.rb
+++ app/models/organization.rb
@@ -24,6 +24,7 @@ class Organization < ApplicationRecord
   has_many :activities, dependent: :destroy
   has_many :hot_springs, dependent: :destroy
   has_many :ski_areas, dependent: :destroy
+  has_many :photo_spots, dependent: :destroy
 
   validates :name, length: { maximum: 100 }, uniqueness: true, presence: true
   validates :address, length: { maximum: 100 }, presence: true
