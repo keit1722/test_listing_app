5d077bb9c5e7e9093f1fc1fb12ffd2b1a0f2d157
keit1722 <keit1722@gmail.com>
2022-05-23 07:13:30 +0900

    [add]photo_spotsコントローラーを作成

---
+++ app/controllers/photo_spots_controller.rb
@@ -0,0 +1,16 @@
+class PhotoSpotsController < ApplicationController
+  layout :determine_layout
+
+  def index
+    @photo_spots =
+      PhotoSpot
+        .with_attached_images
+        .page(params[:page])
+        .per(20)
+        .order(created_at: :desc)
+  end
+
+  def show
+    @photo_spot = PhotoSpot.with_attached_images.find_by(slug: params[:slug])
+  end
+end
