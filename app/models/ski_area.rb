c798623f685e430f774ebc3ec9fdc0c15eb189b1
keit1722 <keit1722@gmail.com>
2022-05-22 10:34:11 +0900

    [add]SkiAraモデルを作成

---
+++ app/models/ski_area.rb
@@ -0,0 +1,53 @@
+# == Schema Information
+#
+# Table name: ski_areas
+#
+#  id              :bigint           not null, primary key
+#  address         :string           not null
+#  description     :text             not null
+#  lat             :float            not null
+#  lng             :float            not null
+#  name            :string           not null
+#  slug            :string           not null
+#  created_at      :datetime         not null
+#  updated_at      :datetime         not null
+#  organization_id :bigint
+#
+# Indexes
+#
+#  index_ski_areas_on_name             (name) UNIQUE
+#  index_ski_areas_on_organization_id  (organization_id)
+#  index_ski_areas_on_slug             (slug) UNIQUE
+#
+# Foreign Keys
+#
+#  fk_rails_...  (organization_id => organizations.id)
+#
+class SkiArea < ApplicationRecord
+  has_many_attached :images
+
+  validates :name, length: { maximum: 100 }, uniqueness: true, presence: true
+  validates :address, length: { maximum: 100 }, presence: true
+  validates :lat, presence: true
+  validates :lng, presence: true
+  validates :slug,
+            length: {
+              maximum: 100,
+            },
+            uniqueness: true,
+            presence: true,
+            format: {
+              with: /\A[a-z0-9\-]+\z/,
+            }
+  validates :description, length: { maximum: 10_000 }, presence: true
+  validates :images,
+            attached: true,
+            limit: {
+              max: 5,
+            },
+            content_type: %i[png jpg jpeg]
+
+  def to_param
+    slug
+  end
+end
