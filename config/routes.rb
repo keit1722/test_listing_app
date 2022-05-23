4e278d21f2e9b7ed96f74650d5c6fe74f3a338c9
keit1722 <keit1722@gmail.com>
2022-05-24 06:26:24 +0900

    [add]Postに関するルーティングを設定

--- config/routes.rb
+++ config/routes.rb
@@ -16,35 +16,56 @@ Rails.application.routes.draw do
 
   resources :organizations, param: :slug do
     scope module: :organization do
-      resources :restaurants, param: :slug
-      resources :shops, param: :slug
-      resources :hotels, param: :slug
-      resources :activities, param: :slug
-      resources :hot_springs, param: :slug
-      resources :ski_areas, param: :slug
-      resources :photo_spots, param: :slug
+      resources :restaurants, param: :slug do
+        resources :posts, module: :restaurants
+      end
+      resources :shops, param: :slug do
+        resources :posts, module: :shops
+      end
+      resources :hotels, param: :slug do
+        resources :posts, module: :hotels
+      end
+      resources :activities, param: :slug do
+        resources :posts, module: :activities
+      end
+      resources :hot_springs, param: :slug do
+        resources :posts, module: :hot_springs
+      end
+      resources :ski_areas, param: :slug do
+        resources :posts, module: :ski_areas
+      end
+      resources :photo_spots, param: :slug do
+        resources :posts, module: :photo_spots
+      end
     end
   end
 
   resources :restaurants, param: :slug, only: %i[index show] do
     resource :bookmarks, only: %i[create destroy], module: :restaurants
+    resources :posts, only: %i[index show], module: :restaurants
   end
   resources :shops, param: :slug, only: %i[index show] do
     resource :bookmarks, only: %i[create destroy], module: :shops
+    resources :posts, only: %i[index show], module: :shops
   end
   resources :hotels, param: :slug, only: %i[index show] do
     resource :bookmarks, only: %i[create destroy], module: :hotels
+    resources :posts, only: %i[index show], module: :hotels
   end
   resources :activities, param: :slug, only: %i[index show] do
     resource :bookmarks, only: %i[create destroy], module: :activities
+    resources :posts, only: %i[index show], module: :activities
   end
   resources :hot_springs, param: :slug, only: %i[index show] do
     resource :bookmarks, only: %i[create destroy], module: :hot_springs
+    resources :posts, only: %i[index show], module: :hot_springs
   end
   resources :ski_areas, param: :slug, only: %i[index show] do
     resource :bookmarks, only: %i[create destroy], module: :ski_areas
+    resources :posts, only: %i[index show], module: :ski_areas
   end
   resources :photo_spots, param: :slug, only: %i[index show] do
     resource :bookmarks, only: %i[create destroy], module: :photo_spots
+    resources :posts, only: %i[index show], module: :photo_spots
   end
 end
