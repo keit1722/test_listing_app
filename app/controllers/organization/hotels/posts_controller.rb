f96284c09dfb46fd404a6abc185d1829dcda78a3
keit1722 <keit1722@gmail.com>
2022-05-24 09:23:11 +0900

    [add]ビジネスに関するpostsコントローラーを作成

---
+++ app/controllers/organization/hotels/posts_controller.rb
@@ -0,0 +1,15 @@
+class Organization::Hotels::PostsController < Organization::PostsController
+  layout 'mypage'
+
+  before_action :set_postable
+
+  private
+
+  def set_postable
+    @postable =
+      current_user
+        .organizations
+        .find_by(slug: params[:organization_slug])
+        .hotels
+        .find_by(slug: params[:hotel_slug])
+  end
---
+++ app/controllers/organization/posts_controller.rb
@@ -0,0 +1,58 @@
+class Organization::PostsController < ApplicationController
+  def index
+    @posts =
+      @postable
+        .posts
+        .with_attached_image
+        .page(params[:page])
+        .per(20)
+        .order(created_at: :desc)
+  end
+
+  def new
+    @post = @postable.posts.build
+  end
+
+  def create
+    @post = @postable.posts.build(post_params)
+    if @post.save
+      redirect_to organization_restaurant_posts_path(
+                    @post.postable.organization,
+                    @post.postable,
+                  ),
+                  success: '登録しました'
+    else
+      flash.now[:error] = '登録に失敗しました'
+      render :new
+    end
+  end
+
+  def edit
+    @post = @postable.posts.find(params[:id])
+  end
+
+  def update
+    @post = @postable.posts.find(params[:id])
+    if @post.update(post_params)
+      redirect_to organization_restaurant_post_path(
+                    @post.postable.organization,
+                    @post.postable,
+                    @post,
+                  ),
+                  success: '投稿を更新しました'
+    else
+      flash.now[:danger] = '投稿の更新に失敗しました'
+      render :edit
+    end
+  end
+
+  def show
+    @post = @postable.posts.find(params[:id])
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :body, :status, :image)
+  end
+end
